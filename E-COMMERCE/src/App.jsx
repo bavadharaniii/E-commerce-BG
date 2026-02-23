e61a7fcb852e3f28aa47dcd27155f314fa73398b
Bavacse2005 <bavadharani1605@gmail.com>
2026-02-23 15:26:50 +0530

    homepage

--- E-COMMERCE/src/App.jsx
+++ E-COMMERCE/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/home.jsx'
 
 
 function App() {
     return(
+        <div>
         <h1>this is my hone page</h1>
+        <Home />
+</div>
     )
 }
 
